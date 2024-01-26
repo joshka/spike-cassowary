75e09edd8135c2f7314f4bc324db5d662b903175
Josh McKinney <joshka@users.noreply.github.com>
2024-01-26 07:39:29 -0800

    mo betta

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,203 @@
-#![allow(dead_code)]
 use std::{
-    collections::{hash_map::RandomState, HashMap},
-    fmt::{Display, Formatter},
+    collections::HashMap,
+    fmt::{Debug, Formatter},
 };
 
 use cassowary::{strength::*, WeightedRelation::*, *};
 use color_eyre::{eyre::eyre, Result};
+use itertools::Itertools;
 
 pub struct Layout {
     solver: Solver,
-    width_variable: Variable,
-    height_variable: Variable,
-    rects: Vec<Rect>,
-    width: f64,
-    height: f64,
+    area_element: Element,
+    elements: Vec<Element>,
+    values: HashMap<Variable, f64>,
 }
 
 #[derive(Debug, Clone, Copy)]
+pub struct Element {
+    x: Variable,
+    y: Variable,
+    width: Variable,
+    height: Variable,
+}
+
+#[derive(Clone, Copy)]
 pub struct Rect {
-    // this could be split into a struct for the variables and a struct for the values
-    x_variable: Variable,
-    y_variable: Variable,
-    width_variable: Variable,
-    height_variable: Variable,
     x: f64,
     y: f64,
     width: f64,
     height: f64,
 }
 
-impl Display for Rect {
+impl Layout {
+    pub fn new(area: Rect) -> Result<Self> {
+        let mut solver = Solver::new();
+        let area_element = Element::new();
+
+        // Edit variables can't be added with strength = REQUIRED
+        // see https://github.com/dylanede/cassowary-rs/issues/2
+        const EDIT_VARIABLE_STRENGTH: f64 = REQUIRED - 1.0;
+        solver
+            .add_edit_variable(area_element.x, EDIT_VARIABLE_STRENGTH)
+            .map_err(|e| eyre!("failed to add x as edit variable: {e:?}"))?;
+        solver
+            .add_edit_variable(area_element.y, EDIT_VARIABLE_STRENGTH)
+            .map_err(|e| eyre!("failed to add y as edit variable: {e:?}"))?;
+        solver
+            .add_edit_variable(area_element.width, EDIT_VARIABLE_STRENGTH)
+            .map_err(|e| eyre!("failed to add width as edit variable: {e:?}"))?;
+        solver
+            .add_edit_variable(area_element.height, EDIT_VARIABLE_STRENGTH)
+            .map_err(|e| eyre!("failed to add height as edit variable: {e:?}"))?;
+        solver
+            .suggest_value(area_element.x, area.x)
+            .map_err(|e| eyre!("failed to set x: {e:?}"))?;
+        solver
+            .suggest_value(area_element.y, area.y)
+            .map_err(|e| eyre!("failed to set y: {e:?}"))?;
+        solver
+            .suggest_value(area_element.width, area.width)
+            .map_err(|e| eyre!("failed to set width: {e:?}"))?;
+        solver
+            .suggest_value(area_element.height, area.height)
+            .map_err(|e| eyre!("failed to set height: {e:?}"))?;
+
+        solver
+            .add_constraints(&[
+                area_element.x | GE(REQUIRED) | 0.0,
+                area_element.y | GE(REQUIRED) | 0.0,
+                area_element.width | GE(REQUIRED) | 0.0,
+                area_element.height | GE(REQUIRED) | 0.0,
+            ])
+            .map_err(|e| eyre!("failed to add generic area constraints: {e:?}"))?;
+
+        let layout = Layout {
+            solver,
+            area_element,
+            elements: Vec::new(),
+            values: HashMap::new(),
+        };
+        Ok(layout)
+    }
+
+    /// stores the rects in the layout and adds constraints to the solver
+    pub fn add_element(&mut self, rect: Element) -> Result<()> {
+        self.elements.push(rect);
+        self.add_constraints(&[
+            rect.left() | GE(REQUIRED) | self.area_element.left(),
+            rect.top() | GE(REQUIRED) | self.area_element.top(),
+            rect.right() | LE(REQUIRED) | self.area_element.bottom(),
+            rect.bottom() | LE(REQUIRED) | self.area_element.bottom(),
+        ])
+    }
+
+    /// adds multiple constraints to the solver
+    pub fn add_constraints(&mut self, constraints: &[Constraint]) -> Result<()> {
+        self.solver
+            .add_constraints(constraints)
+            .map_err(|e| eyre!("failed to add constraints: {e:?}"))
+    }
+
+    /// adds a single constraint to the solver
+    pub fn add_constraint(&mut self, constraint: Constraint) -> Result<()> {
+        self.solver
+            .add_constraint(constraint)
+            .map_err(|e| eyre!("failed to add constraint: {e:?}"))
+    }
+
+    /// fetches the values of the variables from the solver and stores them in the layout
+    /// returns the rects of the elements
+    pub fn get_rects(&mut self) -> Vec<Rect> {
+        let mut rects = Vec::new();
+        let changes = self.solver.fetch_changes();
+        self.values.extend(changes.iter().copied());
+        for element in self.elements.iter() {
+            rects.push(Rect {
+                x: self.value(element.x),
+                y: self.value(element.y),
+                width: self.value(element.width),
+                height: self.value(element.height),
+            });
+        }
+        rects
+    }
+
+    /// helper function to get the value of a variable from the solver
+    fn value(&self, variable: Variable) -> f64 {
+        self.values.get(&variable).copied().unwrap_or(0.0)
+    }
+}
+
+impl Element {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        Element {
+            x: Variable::new(),
+            y: Variable::new(),
+            width: Variable::new(),
+            height: Variable::new(),
+        }
+    }
+
+    fn left(&self) -> Variable {
+        self.x
+    }
+
+    fn right(&self) -> Expression {
+        self.x + self.width
+    }
+
+    fn top(&self) -> Variable {
+        self.y
+    }
+
+    fn bottom(&self) -> Expression {
+        self.y + self.height
+    }
+
+    pub fn precedes_horizontally(&self, other: Element) -> Constraint {
+        self.right() | EQ(REQUIRED) | other.left()
+    }
+
+    pub fn precedes_vertically(&self, other: Element) -> Constraint {
+        self.bottom() | EQ(REQUIRED) | other.top()
+    }
+
+    pub fn has_width(&self, width: f64) -> Constraint {
+        self.width | EQ(STRONG) | width
+    }
+
+    pub fn has_minimum_width(&self, width: f64) -> Constraint {
+        self.width | GE(STRONG) | width
+    }
+
+    pub fn has_maximum_width(&self, width: f64) -> Constraint {
+        self.width | LE(STRONG) | width
+    }
+
+    pub fn has_proportional_width(&self, other: Element, ratio: f64) -> Constraint {
+        (self.width / ratio) | EQ(MEDIUM) | other.width
+    }
+
+    pub fn has_height(&self, height: f64) -> Constraint {
+        self.height | EQ(STRONG) | height
+    }
+
+    pub fn has_minimum_height(&self, height: f64) -> Constraint {
+        self.height | GE(STRONG) | height
+    }
+
+    pub fn has_maximum_height(&self, height: f64) -> Constraint {
+        self.height | LE(STRONG) | height
+    }
+
+    pub fn has_proportional_height(&self, other: Element, ratio: f64) -> Constraint {
+        (self.height / ratio) | EQ(MEDIUM) | other.height
+    }
+}
+
+impl Debug for Rect {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
@@ -39,129 +207,52 @@ impl Display for Rect {
     }
 }
 
-impl Layout {
-    pub fn new() -> Result<Self> {
-        let mut solver = Solver::new();
-        let width = Variable::new();
-        let height = Variable::new();
-        solver
-            .add_edit_variable(width, REQUIRED - 1.0) // https://github.com/dylanede/cassowary-rs/issues/2
-            .map_err(|e| eyre!("failed to add width: {e:?}"))?;
-        solver
-            .add_edit_variable(height, REQUIRED - 1.0)
-            .map_err(|e| eyre!("failed to add height: {e:?}"))?;
-        solver
-            .add_constraints(&[width | GE(REQUIRED) | 0.0, height | GE(REQUIRED) | 0.0])
-            .map_err(|e| eyre!("failed to add constraints: {e:?}"))?;
-        Ok(Layout {
-            solver,
-            width_variable: width,
-            height_variable: height,
-            rects: Vec::new(),
-            width: 0.0,
-            height: 0.0,
-        })
-    }
-
-    pub fn add_rect(&mut self, rect: Rect) -> Result<()> {
-        self.add_constraints(&[
-            rect.x_variable | GE(REQUIRED) | 0.0,
-            rect.y_variable | GE(REQUIRED) | 0.0,
-            rect.x_variable + rect.width_variable | LE(REQUIRED) | self.width_variable,
-            rect.y_variable + rect.height_variable | LE(REQUIRED) | self.height_variable,
-        ])?;
-        self.rects.push(rect);
-        Ok(())
-    }
-
-    pub fn add_constraints(&mut self, constraints: &[Constraint]) -> Result<()> {
-        self.solver
-            .add_constraints(constraints)
-            .map_err(|e| eyre!("failed to add constraints: {e:?}"))?;
-        Ok(())
-    }
-
-    pub fn add_constraint(&mut self, constraint: Constraint) -> Result<()> {
-        self.solver
-            .add_constraint(constraint)
-            .map_err(|e| eyre!("failed to add constraint: {e:?}"))?;
-        Ok(())
-    }
-
-    pub fn set_size(&mut self, width: f64, height: f64) -> Result<()> {
-        self.solver
-            .suggest_value(self.width_variable, width)
-            .map_err(|e| eyre!("failed to set width: {e:?}"))?;
-        self.solver
-            .suggest_value(self.height_variable, height)
-            .map_err(|e| eyre!("failed to set height: {e:?}"))?;
-        let changes = self.solver.fetch_changes();
-        let map: HashMap<_, _, RandomState> = HashMap::from_iter(changes.iter().copied());
-        for rect in &mut self.rects {
-            rect.x = *map.get(&rect.x_variable).unwrap_or(&0.0);
-            rect.y = *map.get(&rect.y_variable).unwrap_or(&0.0);
-            rect.width = *map.get(&rect.width_variable).unwrap_or(&0.0);
-            rect.height = *map.get(&rect.height_variable).unwrap_or(&0.0);
-        }
-        Ok(())
-    }
-}
-
-impl Rect {
-    pub fn new() -> Self {
-        Rect {
-            x: 0.0,
-            y: 0.0,
-            width: 0.0,
-            height: 0.0,
-            x_variable: Variable::new(),
-            y_variable: Variable::new(),
-            width_variable: Variable::new(),
-            height_variable: Variable::new(),
-        }
-    }
-
-    fn precedes_horizontally(&self, other: Rect) -> Constraint {
-        self.x_variable + self.width_variable | EQ(REQUIRED) | other.x_variable
-    }
-
-    fn precedes_vertically(&self, other: Rect) -> Constraint {
-        self.y_variable + self.height_variable | EQ(REQUIRED) | other.y_variable
-    }
-
-    pub fn has_width(&self, width: f64) -> Constraint {
-        self.width_variable | EQ(STRONG) | width
-    }
-
-    fn has_proportional_width(&self, other: Rect, ratio: f64) -> Constraint {
-        self.width_variable / ratio | EQ(MEDIUM) | other.width_variable
-    }
-}
-
 fn main() -> Result<()> {
-    let mut layout = Layout::new()?;
-    let rects = vec![Rect::new(), Rect::new(), Rect::new()];
-    for rect in rects.iter() {
-        layout.add_rect(*rect)?;
+    let area = Rect {
+        x: 0.0,
+        y: 0.0,
+        width: 50.0,
+        height: 50.0,
+    };
+
+    let mut layout = Layout::new(area)?;
+    let elements = [Element::new(), Element::new(), Element::new()];
+
+    for element in elements.iter() {
+        layout.add_element(*element)?;
     }
 
-    layout.add_constraint(rects[0].precedes_horizontally(rects[1]))?;
-    layout.add_constraint(rects[1].precedes_horizontally(rects[2]))?;
-    layout.add_constraint(rects[0].has_width(60.0))?;
-    layout.add_constraint(rects[1].has_width(30.0))?;
-    layout.add_constraint(rects[2].has_width(10.0))?;
-    layout.add_constraint(rects[0].has_proportional_width(rects[1], 60.0 / 30.0))?;
-    layout.add_constraint(rects[1].has_proportional_width(rects[2], 30.0 / 10.0))?;
+    let widths = [60.0, 30.0, 10.0];
 
-    layout.set_size(50.0, 50.0)?;
-
-    println!(
-        "Layout {{ width: {}, height: {} }}",
-        layout.width, layout.height
-    );
-    for rect in &layout.rects {
-        println!("{}", rect);
+    // set the width of each element
+    for (&element, width) in elements.iter().zip(widths.iter()) {
+        layout.add_constraint(element.has_width(*width))?;
     }
+
+    // arrange elements horizontally
+    for (&left, &right) in elements.iter().tuple_windows() {
+        layout.add_constraint(left.precedes_horizontally(right))?;
+    }
+
+    // ensure that all elements are scaled proportionally if there is not enough space remaining
+    for ((left, left_width), (right, right_width)) in
+        elements.iter().zip(widths.iter()).tuple_windows()
+    {
+        layout.add_constraint(left.has_proportional_width(*right, *left_width / *right_width))?;
+    }
+
+    println!("widths: {:?}", widths);
+    println!("area:\n    {:?}", area);
+    println!("rects: {:#?}", layout.get_rects());
+    // prints:
+    // widths: [60.0, 30.0, 10.0]
+    // area:
+    //     Rect { x:  0.0, y:  0.0, width: 50.0, height: 50.0 }
+    // rects: [
+    //     Rect { x:  0.0, y:  0.0, width: 30.0, height: 50.0 },
+    //     Rect { x: 30.0, y:  0.0, width: 15.0, height: 50.0 },
+    //     Rect { x: 45.0, y:  0.0, width:  5.0, height: 50.0 },
+    // ]
 
     Ok(())
 }
